772ad4b0bda91737fc3a29677e41267a3ebd4a72
Guben-06 <ruben.sobrin06@gmail.com>
2026-02-07 17:01:05 +0100

    Add files via upload

---
+++ scripts/contrasena.js
@@ -0,0 +1,13 @@
+<script languaje="javascript">
+
+  var getin = prompt("Pon la contraseña.")
+
+  if (getin!="012106")
+
+  {location.href='index.html'}
+
+  else
+
+  {alert('Contraseña correcta, acepta para ver la entrada')}
+
+</script>
